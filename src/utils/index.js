e284bda43a49a89d6460aa8f4f3ac7dfea30fa79
Barak Drechsler <barak.drechsler@teamaol.com>
2017-04-02 06:57:34 +0300

    adding guidUtil

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+/**
+ * Generates a unique id (a 1-in-a-million chance of collision), used to mock unique ids
+ * for object creations, as in real world case, those ids will be determined by the server
+ * and not the client
+ */
+export const guidGenerator = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+});
